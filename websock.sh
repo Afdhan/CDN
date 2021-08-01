4ddf143f823a0985e541ceb4ef75769ae61eff31
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-01 23:38:56 +0700

    F

--- websock.sh
+++ websock.sh
@@ -5,10 +5,12 @@ cd /root
 figlet -f slant Install WS | lolcat
 # Install Template
 wget -q -O /usr/local/bin/ws-drop "https://raw.githubusercontent.com/Afdhan/CDN/main/ws-dropbear.py"
+wget -q -O /usr/local/bin/ws-openssh "https://raw.githubusercontent.com/Afdhan/CDN/main/ws-openssh.py"
 wget -q -O /usr/local/bin/ws-ovpn "https://raw.githubusercontent.com/Afdhan/CDN/main/ws-ovpn.py"
 wget -q -O /usr/local/bin/ws-tls "https://raw.githubusercontent.com/Afdhan/CDN/main/ws-tls.py"
 
 chmod +x /usr/local/bin/ws-drop
+chmod +x /usr/local/bin/ws-openssh
 chmod +x /usr/local/bin/ws-ovpn
 chmod +x /usr/local/bin/ws-tls
 
@@ -28,6 +30,26 @@ CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
 NoNewPrivileges=true
 ExecStart=/usr/bin/python -O /usr/local/bin/ws-drop
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# OpenSSH
+cat > /etc/systemd/system/ws-openssh.service << END
+[Unit]
+Description=SSH Over CDN WS Dropbear
+Documentation=https://dhans-project.xyz
+After=network.target nss-lookup.target
+
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-openssh
 Restart=on-failure
 
 [Install]
@@ -79,11 +101,13 @@ systemctl daemon-reload
 
 # Activated
 systemctl enable ws-dropbear.service
+systemctl enable ws-openssh.service
 systemctl enable ws-openvpn.service
 systemctl enable ws-stunnel.service
 
 # Restatt
 systemctl restart ws-dropbear.service
+systemctl restart ws-openssh.service
 systemctl restart ws-openvpn.service
 systemctl restart ws-stunnel.service
 
