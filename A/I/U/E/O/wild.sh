a5ea7c8184be192a62fecbe2a1a6747c693e5967
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-19 12:25:24 +0700

    F

--- A/I/U/E/O/wild.sh
+++ A/I/U/E/O/wild.sh
@@ -2,40 +2,9 @@
 
 domin=$(cat /root/domain)
 
-SUB_DOMIN=*.${domin}
-CF_ID=neza.afdhan@gmail.com
-CF_KEY=c7ce6739f7548dcb626dcbee71140345f2625
-set -euo pipefail
-IP=$(wget -qO- ipinfo.io/ip);
-
-ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${domin}&status=active" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMIN}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" | jq -r .result[0].id)
-
-if [[ "${#RECORD}" -le 10 ]]; then
-     RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
-fi
-
-RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-     -H "X-Auth-Email: ${CF_ID}" \
-     -H "X-Auth-Key: ${CF_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"type":"A","name":"'${SUB_DOMIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
-
-
 cat > /etc/nginx/sites-enabled/wildcard_subdomain << END
 server {
-    server_name ${SUB_DOMIN};
+    server_name *.${domin};
     access_log /var/log/nginx/vps-access.log;
     error_log /var/log/nginx/vps-error.log error;
     root /home/vps/public_html;
@@ -71,5 +40,6 @@ server {
 END
 #listen 443 ssl;
   #  ssl_certificate /etc/v2ray/v2ray.crt;
- #   ssl_certificate_key /etc/v2ray/v2ray.key; 
+ #   ssl_certificate_key /etc/v2ray/v2ray.key;
+ systemctl daemon-reload
  systemctl restart nginx
