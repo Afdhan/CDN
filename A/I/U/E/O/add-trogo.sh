a707021f57e23c1922f9442c11da3ceeaba1b639
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-22 07:16:25 +0700

    F

---
+++ A/I/U/E/O/add-trogo.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+grey='\x1b[90m'
+red='\x1b[91m'
+green='\x1b[92m'
+yellow='\x1b[93m'
+blue='\x1b[94m'
+purple='\x1b[95m'
+cyan='\x1b[96m'
+white='\x1b[37m'
+bold='\033[1m'
+off='\x1b[m'
+flag='\x1b[47;41m'
+
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+CITY=$(curl -s ipinfo.io/city )
+COUNTRY=$(curl -s ipinfo.io/country )
+
+MYIP=$(wget -qO- ipinfo.io/ip);
+IZIN=$( curl https://afdhan.github.io/sce/izin | grep $MYIP )
+echo "Memeriksa Hak Akses VPS..."
+if [ $MYIP = $IZIN ]; then
+clear
+echo -e "${green}Akses Diizinkan...${off}"
+sleep 1
+else
+clear
+echo -e "${red}Akses Diblokir!${off}"
+echo "Hanya Untuk Pengguna Berbayar!"
+echo "Silahkan Hubungi Admin"
+exit 0
+fi
+clear
+uuid=$(cat /proc/sys/kernel/random/uuid)
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+tr=1995
+#tr="$(cat ~/log-install.txt | grep -i Trojan | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
+		read -rp "Remarks: " -e user
+		user_EXISTS=$(grep -w $user /etc/go-go/akun.conf | wc -l)
+
+		if [[ ${user_EXISTS} == '1' ]]; then
+			echo ""
+			echo "Nama User Sudah Ada, Harap Masukkan Nama Lain!"
+			exit 1
+		fi
+	done
+	
+read -p "Expired (hari): " masaaktif
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/go-go/config.json
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+tgl=$(date -d "$masaaktif days" +"%d")
+bln=$(date -d "$masaaktif days" +"%b")
+thn=$(date -d "$masaaktif days" +"%Y")
+expe="$tgl $bln, $thn"
+tgl2=$(date +"%d")
+bln2=$(date +"%b")
+thn2=$(date +"%Y")
+tnggl="$tgl2 $bln2, $thn2"
+echo -e "### $user $exp" >> /etc/go-go/akun.conf
+systemctl restart trojan-go
+trojanlink="trojan-go://${uuid}@IP_BUG:${tr}/?sni=${domain}&type=ws&host=${domain}&path=/go-go&encryption=none#${user}"
+clear
+echo -e ""
+echo -e "${cyan}=================================${off}"
+echo -e "${purple} ~> TROJAN VPN${off}"
+echo -e "${cyan}=================================${off}"
+echo -e " ${green}ISP            : ${ISP}"
+echo -e " CITY           : ${CITY}"
+echo -e " COUNTRY        : ${COUNTRY}"
+echo -e " Server IP      : ${MYIP}"
+echo -e " Remarks        : ${user}"
+echo -e " Host           : ${domain}"
+echo -e " Port           : ${tr}"
+echo -e " Key            : ${uuid}"
+echo -e " Link           : ${trojanlink}${off}"
+echo -e "${cyan}=================================${off}"
+echo -e " ${green}Aktif Selama   : $masaaktif Hari"
+echo -e " Dibuat Pada    : $tnggl"
+echo -e " Berakhir Pada  : $expe${off}"
+echo -e "${cyan}=================================${off}"
+echo -e " ${purple}- Mod By Dhansss X NezaVPN${off}"
+echo -e ""
---
+++ A/I/U/E/O/trojango.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+# Trojan Go Auto Setup 
+# =========================
+
+# Domain 
+domain=$(cat /etc/v2ray/domain)
+
+# Uuid Service
+uuid=$(cat /proc/sys/kernel/random/uuid)
+
+# Trojan Go Akun 
+mkdir -p /etc/go-go/
+touch /etc/go-go/akun.conf
+
+# Installing Trojan Go
+mkdir -p /etc/go-go/
+chmod 755 /etc/go-go/
+touch /etc/go-go/trojan-go.pid
+wget -O /usr/local/bin/trojan-go https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/trojan-go
+wget -O /usr/local/bin/geoip.dat https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/geoip.dat
+wget -O /usr/local/bin/geosite.dat https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/geosite.dat
+chmod +x /usr/local/bin/trojan-go
+
+# Service
+cat > /etc/systemd/system/trojan-go.service << END
+[Unit]
+Description=Trojan-Go - An unidentifiable mechanism that helps you bypass GFW
+Documentation=https://p4gefau1t.github.io/trojan-go/
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+NoNewPrivileges=true
+ExecStart=/usr/local/bin/trojan-go -config /etc/go-go/config.json
+Restart=on-failure
+RestartSec=10s
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target
+END
+
+# Config
+cat > /etc/go-go/config.json << END
+{
+    "run_type": "server",
+    "local_addr": "0.0.0.0",
+    "local_port": 1995,
+    "remote_addr": "127.0.0.1",
+    "remote_port": 81,
+    "password": [
+        "$uuid"
+    ],
+    "ssl": {
+        "cert": "/etc/v2ray/v2ray.crt", 
+        "key": "/etc/v2ray/v2ray.key", 
+        "sni": "$domain"
+    },
+    "router": {
+        "enabled": true,
+        "block": [
+            "geoip:private"
+        ],
+        "geoip": "/usr/local/bin/geoip.dat",
+        "geosite": "/usr/local/bin/geosite.dat"
+    }
+}
+END
+
+iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport 1995 -j ACCEPT
+iptables -I INPUT -m state --state NEW -m udp -p udp --dport 1995 -j ACCEPT
+iptables-save > /etc/iptables.up.rules
+iptables-restore -t < /etc/iptables.up.rules
+netfilter-persistent save
+netfilter-persistent reload
+
+# Starting
+systemctl daemon-reload
+systemctl enable trojan-go
+systemctl start trojan-go
+
+wget -O /usr/bin/trgo https://raw.githubusercontent.com/Afdhan/CDN/main/A/I/U/E/O/add-trogo.sh
+chmod +x /usr/bin/trgo
