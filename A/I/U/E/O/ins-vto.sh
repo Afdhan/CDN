9ca8c70507d016c8093cd097cb181ec5b5f1763b
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-08-24 05:25:44 +0700

    F

--- A/I/U/E/O/ins-vto.sh
+++ A/I/U/E/O/ins-vto.sh
@@ -58,7 +58,7 @@ cat> /etc/v2ray/config.json << END
           ]
         },
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/DhanZaaProject",
           "headers": {
             "Host": ""
           }
@@ -146,7 +146,7 @@ cat> /etc/v2ray/none.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/DhanZaaProject",
           "headers": {
             "Host": ""
           }
@@ -243,7 +243,7 @@ cat> /etc/v2ray/vless.json << END
           ]
         },
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/DhanZaaProject",
           "headers": {
             "Host": ""
           }
@@ -330,7 +330,7 @@ cat> /etc/v2ray/vnone.json << END
       "streamSettings": {
         "network": "ws",
         "wsSettings": {
-          "path": "/v2ray",
+          "path": "/DhanZaaProject",
           "headers": {
             "Host": ""
           }
