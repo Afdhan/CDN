1fad27f88e22617456f690c376b3bc5e2c19e247
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-11-23 19:54:14 +0700

    Create vmess.sh

---
+++ vmess.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+clear
+source /var/lib/premium-script/ipvps.conf
+if [[ "$IP" = "" ]]; then
+domain=$(cat /etc/v2ray/domain)
+else
+domain=$IP
+fi
+tls="$(cat ~/log-install.txt | grep -w "V2RAY Vmess TLS" | cut -d: -f2|sed 's/ //g')"
+none="$(cat ~/log-install.txt | grep -w " V2RAY Vmess None TLS" | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Remarks: " -e user
+		CLIENT_EXISTS=$(grep -w $user /etc/v2ray/config.json | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '2' ]]; then
+			echo ""
+			echo "Nama User Sudah Ada, Silahkan Gunakan Nama lain"
+			exit 1
+		fi
+	done
+uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (days): " masaaktif
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+sed -i '/#tls$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/config.json
+sed -i '/#none$/a\### '"$user $exp"'\
+},{"id": "'""$uuid""'","alterId": '"2"',"email": "'""$user""'"' /etc/v2ray/none.json
+cat>/etc/v2ray/$user-tls.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${tls}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/WorldSSH",
+      "type": "none",
+      "host": "",
+      "tls": "tls"
+}
+EOF
+cat>/etc/v2ray/$user-none.json<<EOF
+      {
+      "v": "2",
+      "ps": "${user}",
+      "add": "${domain}",
+      "port": "${none}",
+      "id": "${uuid}",
+      "aid": "2",
+      "net": "ws",
+      "path": "/WorldSSH",
+      "type": "none",
+      "host": "${domain}",
+      "tls": "none"
+}
+EOF
+vmess_base641=$( base64 -w 0 <<< $vmess_json1)
+vmess_base642=$( base64 -w 0 <<< $vmess_json2)
+vmesslink1="vmess://$(base64 -w 0 /etc/v2ray/$user-tls.json)"
+vmesslink2="vmess://$(base64 -w 0 /etc/v2ray/$user-none.json)"
+systemctl restart v2ray
+systemctl restart v2ray@none
+service cron restart
+clear
+echo -e ""
+echo -e "=========[ V2RAY/VMESS ]=========" | lolcat
+echo -e "Remarks        : ${user}"
+echo -e "Domain         : ${domain}"
+echo -e "port TLS       : ${tls}"
+echo -e "port none TLS  : ${none}"
+echo -e "id             : ${uuid}"
+echo -e "alterId        : 2"
+echo -e "Security       : auto"
+echo -e "network        : ws"
+echo -e "path           : /WorldSSH"
+echo -e "=================================" | lolcat
+echo -e "link TLS       : ${vmesslink1}"
+echo -e "=================================" | lolcat
+echo -e "link none TLS  : ${vmesslink2}"
+echo -e "=================================" | lolcat
+echo -e "Aktif Selama   : ${masaaktif} Hari"
+echo -e "Berakhir Pada  : ${exp}"
+echo -e "Mod By Dhansss & NezaVPN"
