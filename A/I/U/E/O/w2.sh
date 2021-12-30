1bf8aaa8a94947d5f5ced8149d7b0a857feec6ef
Afdhan <77567725+Afdhan@users.noreply.github.com>
2021-12-30 20:06:46 +0700

    Update w2.sh

--- A/I/U/E/O/w2.sh
+++ A/I/U/E/O/w2.sh
@@ -19,95 +19,63 @@ set -euo pipefail
 IP=$(wget -qO- ipinfo.io/ip);
 
 ZONE=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones?name=${DOMAIN}&status=active" \
-
      -H "X-Auth-Email: ${CF_ID}" \
-
      -H "X-Auth-Key: ${CF_KEY}" \
-
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
 RECORD=$(curl -sLX GET "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records?name=${SUB_DOMAIN}" \
-
      -H "X-Auth-Email: ${CF_ID}" \
-
      -H "X-Auth-Key: ${CF_KEY}" \
-
      -H "Content-Type: application/json" | jq -r .result[0].id)
 
 if [[ "${#RECORD}" -le 10 ]]; then
 
      RECORD=$(curl -sLX POST "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records" \
-
      -H "X-Auth-Email: ${CF_ID}" \
-
      -H "X-Auth-Key: ${CF_KEY}" \
-
      -H "Content-Type: application/json" \
-
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}' | jq -r .result.id)
 
 fi
 
 RESULT=$(curl -sLX PUT "https://api.cloudflare.com/client/v4/zones/${ZONE}/dns_records/${RECORD}" \
-
      -H "X-Auth-Email: ${CF_ID}" \
-
      -H "X-Auth-Key: ${CF_KEY}" \
-
      -H "Content-Type: application/json" \
-
      --data '{"type":"A","name":"'${SUB_DOMAIN}'","content":"'${IP}'","ttl":120,"proxied":false}')
 
 cat > /etc/nginx/sites-enabled/wildcard_subdomain << END
 
 server {
-
     server_name ${SUB_DOMAIN};
-
     access_log /var/log/nginx/vps-access.log;
-
     error_log /var/log/nginx/vps-error.log error;
-
-    root /home/vps/public_html;
-
-    
+    root /home/vps/public_html;  
 
     location / {
-
        index  index.html index.htm index.php;
-
        try_files $uri $uri/ /index.php?$args;
-
     }
 
     location ~\.php$ {
-
         include /etc/nginx/fastcgi_params;
-
         fastcgi_pass  127.0.0.1:9000;
-
         fastcgi_index index.php;
-
         fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-
     }
 
     location ~ /\.ht {
-
-        deny all;
+      deny all;
 
     }
 
     location ~* /(?:uploads|files)/.*\.php$ {
-
         deny all;
 
     }
 
     location ~* \.(jpg|jpeg|gif|png|ico|cur|gz|svg|svgz|mp4|ogg|ogv|webm|htc)$ {
-
          expires 356d;
-
          add_header Cache-Control "public, no-transform";
 
     }
@@ -117,15 +85,11 @@ server {
     
 
     location ~* \.(css|js)$ {
-
          expires 2d;
-
          add_header Cache-Control "public, no-transform";
 
     }
-
     listen: 81;
-
 }
 
 END
